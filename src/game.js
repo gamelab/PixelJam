c8b254f6788f1d100ea1c8be7dc68f42336e8cde
BenjaminHarding <benjamin.p.harding@gmail.com>
2014-09-12 18:19:11 +1200

    Pixel Jam initial commit

---
+++ src/game.js
@@ -0,0 +1,25 @@
+
+/**
+* The core PixelJam game file.
+* 
+* This file is only used to initalise (start-up) the main Kiwi Game 
+* and add all of the relevant states to that Game.
+*
+*/
+
+//Initialise the Kiwi Game. 
+
+var gameOptions = {
+	renderer: Kiwi.RENDERER_WEBGL, 
+	width: 800,
+	height: 600
+}
+
+var game = new Kiwi.Game('content', 'PixelJam', null, gameOptions);
+
+//Add all the States we are going to use.
+game.states.addState(PixelJam.Loading);
+game.states.addState(PixelJam.Intro);
+game.states.addState(PixelJam.Play);
+
+game.states.switchState("Loading");
